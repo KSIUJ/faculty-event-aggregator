845b4dedfd8e4864513caed925ccf743f1d198d3
Szymon <zwiasszymon@gmail.com>
2026-07-24 16:37:22 +0200

    chore: create SQLAlchemy engine and session factory
     Create the SQLAlchemy engine and SessionLocal objects inside database.py.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from config import settings
 
 
 DATABASE_URL = (
     f"postgresql://{settings.DB_USER}:{settings.DB_PASS}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
 )
