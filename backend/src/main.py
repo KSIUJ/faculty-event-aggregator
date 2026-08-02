838d5bacde8444fdfff156b6b0c3c148822ad080
Maciej <230854084+maciejzujtu@users.noreply.github.com>
2026-08-02 17:15:58 +0200

    feat(backend): Enable CORS
    - Added the `CORS_ORIGINS` environment variable for easy configuration of allowed dev origins.
    
    Note: Waiting for #32 PR to merge before creating a PR for this

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,20 @@
+from config import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.events import router as events_router
-
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(events_router)
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!" }
-
+    return {"message": "Hello World!"}
