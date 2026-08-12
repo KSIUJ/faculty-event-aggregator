9329022c77ae827ce1ff00e8da44469a3d24e89e
Maciej <230854084+maciejzujtu@users.noreply.github.com>
2026-08-12 13:40:53 +0200

    updated(backend): Updated main
    Backend server class now includes method configuring CORS middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from config import settings
 from routers.event_categories import router as event_categories_router
 from routers.events import router as events_router
 from routers.organizers import router as organizers_router
 from routers.topic_categories import router as topic_categories_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(event_categories_router)
