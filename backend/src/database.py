085c58a0ae5f3f9c31d5e50fd56175554688a396
Maciej <230854084+maciejzujtu@users.noreply.github.com>
2026-08-19 18:30:13 +0200

    fix(requirements): Updated libs
    - Changed `psycopg2` to `psycopg` to avoid error when installing packages on newly created env
    - Added `pytest` to utilize tests in `tests` folder
    - Added `httpx` as a dependecy
    - Updated `DATABASE_URL` to work with new psycopg lib.

--- backend/src/database.py
+++ backend/src/database.py
@@ -6,7 +6,7 @@ from config import settings
 
 
 DATABASE_URL = (
-    f"postgresql://{settings.DB_USER}:{settings.DB_PASS}"
+    f"postgresql+psycopg://{settings.DB_USER}:{settings.DB_PASS}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 
