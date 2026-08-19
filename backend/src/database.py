375635b8fbd100aa7f8174b8fe5083b09cb0cff1
Maciej <230854084+maciejzujtu@users.noreply.github.com>
2026-08-19 18:00:13 +0200

    refactor(backend): Moved `database.py` to `src`
    Implemented utility functions that are necessary for CRUD router functions.

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,5 +1,6 @@
+from typing import TypeVar
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from config import settings
 
@@ -18,9 +19,24 @@ SessionLocal = sessionmaker(
     bind=engine,
 )
 
+ModelType = TypeVar("ModelType")
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+# Util functions
+def commit(db: Session) -> None:
+    try:
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+
+def commit_and_refresh(db: Session, instance: ModelType) -> ModelType:
+    commit(db)
+    db.refresh(instance)
+    return instance
