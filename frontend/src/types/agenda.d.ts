15ca07f7dc92ec31bd3f5108fb5387e2982fabf8
Maciej <230854084+maciejzujtu@users.noreply.github.com>
2026-08-28 01:42:54 +0200

    change(frontend): rename one of AgendaDateRange's values

--- frontend/src/types/agenda.d.ts
+++ frontend/src/types/agenda.d.ts
@@ -1,7 +1,10 @@
 export type AgendaCategory = number | 'all'
-export type AgendaDateRange = 'upcoming' | 'today' | 'tomorrow' | 'week' | 'next7' | 'next30' | 'custom'
+export type AgendaDateRange = 'all' | 'today' | 'tomorrow' | 'week' | 'next7' | 'next30' | 'custom'
 
 export interface AgendaCustomDateRange {
     start: string
     end: string
 }
+
+export type AgendaEventStatus = 'all' | 'not-started' | 'ongoing' | 'ended'
+export type EventLifecycleStatus = Exclude<AgendaEventStatus, 'all'>
